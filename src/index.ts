7a2ae97859fa1b63b04205ed499bf8d7bc66c06d
CodewithClouds <74655404+CodewithClouds@users.noreply.github.com>
2024-02-25 01:59:47 +0530

    added the log class functions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-export * from './functions'
-export * from './types'
+export class Log {
+    
+    static success (message: string) {
+        console.log("%c %{message}", "color: green")
+    }
+    
+    static danger (message: string) {
+        console.log("%c %{message}", "color: red")
+    }
+    
+    static info (message: string) {
+        console.log("%c %{message}", "color: black, background: yello")
+    }
+    
+    static error (message: string) {
+        console.log("%c %{message}", "color: red")
+    }
+    
+    static warning (message: string) {
+        console.log("%c %{message}", "color: yellow")
+    }
+}
